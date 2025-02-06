a5ded221353cd6757c2ed7d154e485d788ce18ce
Amandine Ameye <amandineameye@gmail.com>
2025-02-06 22:45:55 +0100

    Put config function of diabestie

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
-	return {
-		define: {
-			"process.env.REACT_APP_API_URL": JSON.stringify(env.REACT_APP_API_URL),
-		},
-		plugins: [react()],
-	};
+export default defineConfig({
+	plugins: [react()],
 });
