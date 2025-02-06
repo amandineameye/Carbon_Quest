afcc677e79bbeede146274c98ba9464f4c09d8b5
Amandine Ameye <amandineameye@gmail.com>
2025-02-06 19:12:56 +0100

    Gave color to the score

--- frontend/src/components/ScoreResult/ScoreResult.jsx
+++ frontend/src/components/ScoreResult/ScoreResult.jsx
@@ -1,10 +1,12 @@
 import { scoreData } from "../../data/scoreData.js";
 
-const ScoreCard = ({ title, bgColor, message, score }) => (
+const ScoreCard = ({ title, titleColor, nbColor, message, score }) => (
 	<div>
-		<h3 className={`p-1 rounded-sm ${bgColor}`}>{title}</h3>
+		<h3 className={`p-1 rounded-sm ${titleColor}`}>{title}</h3>
 		<p className="text-lg mt-6">{message}</p>
-		<div className="mt-6 rounded-full mx-auto w-20 h-20 flex items-center justify-center text-4xl font-bold">
+		<div
+			className={`mt-6 rounded-full mx-auto w-20 h-20 flex items-center justify-center text-white text-4xl font-bold ${nbColor}`}
+		>
 			{score}
 		</div>
 	</div>
@@ -22,7 +24,8 @@ const ScoreResult = ({ currentScore }) => {
 		scoreInfo && (
 			<ScoreCard
 				title={scoreInfo.title}
-				bgColor={scoreInfo.bgColor}
+				titleColor={scoreInfo.titleColor}
+				nbColor={scoreInfo.nbColor}
 				message={scoreInfo.message}
 				score={currentScore}
 			/>
