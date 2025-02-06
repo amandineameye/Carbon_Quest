d74717baf1e82c9ec05fdf49a9daeb95266bfe38
Amandine Ameye <amandineameye@gmail.com>
2025-02-06 21:30:48 +0100

    Old version of vercel.json + require dotenv + cors middleware

--- backendNode/index.js
+++ backendNode/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-// require("dotenv").config();
+require("dotenv").config();
 
-// app.use(
-// 	cors({
-// 		origin: "https://super-carbon-quest.vercel.app/", // Your frontend URL
-// 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
-// 		credentials: true, // Allow cookies if needed
-// 	})
-// );
+app.use(
+	cors({
+		origin: "https://super-carbon-quest.vercel.app", // Your frontend URL
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
+		credentials: true, // Allow cookies if needed
+	})
+);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
