5c446c1c5bef0f537a31b624329174baf9c7043e
Amandine Ameye <amandineameye@gmail.com>
2025-03-23 22:21:08 +0100

    Removed PHP backend (moved to legacy-php-backend branch)

--- backendPHP/app/config/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const DSN = "mysql:host=localhost;dbname=carbonquest;port=3306;charset=utf8";
-const USERNAME = "root";
-const PASSWORD = "";        // XAMPP
-// const PASSWORD = "root";    // MAMP
